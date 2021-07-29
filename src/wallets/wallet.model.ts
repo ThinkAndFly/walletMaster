7f1e8ada72056b26d4bc0c28bb47eb589fb55bde
Juan Di Minico <juandiminico@gmail.com>
2021-07-29 20:15:53 +0200

    added validations. API finished v1

--- src/wallets/wallet.model.ts
+++ src/wallets/wallet.model.ts
@@ -4,10 +4,12 @@ export const WalletSchema = new mongoose.Schema({
   address: { type: String, required: true },
   description: { type: String, required: false },
   favorite: { type: Boolean, required: true },
+  firstTransaction: {type: Date, required: false}
 });
 
 export interface Wallet extends mongoose.Document {
   address: string;
   description: string;
   favorite: boolean;
+  firstTransaction: Date;
 }
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -9,61 +9,45 @@ import { AxiosResponse } from 'axios';
 @Injectable()
 export class WalletsService {
   private apiKEY: string = 'NSZCD6S4TKVWRS13PMQFMVTNP6H7NAGHUY';
-  private balanceToEth: number = 1000000000000000000;
+  private weiToEth: number = Math.pow(10, 18);
+
   constructor(
     @InjectModel('Wallet') private readonly walletModel: Model<Wallet>,
     private httpService: HttpService,
   ) {}
-
-  async getWalletBalanceWithEtherscan(address: string) {
-    var balance : number;
-    const url =
-      'https://api.etherscan.io/api?module=account&action=balance&address=' +
-      address +
-      '&tag=latest&apikey=' +
-      this.apiKEY +
-      '';
-    const wallet = await this.httpService.get<AxiosResponse<BasicResult>>(url);
-    await wallet.forEach((w) => {
-        const rawBalance = (w.data as unknown as BasicResult).result;
-        balance = parseInt(rawBalance) / this.balanceToEth;
-    });
-    return balance; 
-  }
-
-  async getWalletFirstUseWithEtherscan(address: string) : Promise<Date> {
-    const url =
-      'https://api.etherscan.io/api?module=account&action=txlist&address=' +
-      address +
-      '&startblock=0&endblock=99999999&sort=asc&apikey=' +
-      this.apiKEY +
-      '';
-      
-    var date : Date;
-    const wallet = await this.httpService.get<AxiosResponse<WalletResult>>(url);
-    await wallet.forEach((w) => {
-        const firstUse = (w.data as unknown as WalletResult).result[0].timeStamp;
-        date = new Date(parseInt(firstUse)*1000);
-    });
-    return (date); 
-  }
-
+  
   async insertWallet(address: string, description: string) {
+    const date = await this.getWalletFirstUseWithEtherscan(address);
+    
     const newWallet = new this.walletModel({
       address: address,
       description: description,
       favorite: false,
+      firstTransaction: date ?? null
     });
+
     const result = await newWallet.save();
   }
 
   async getAllWallets() {
     const wallets = await this.walletModel.find().exec();
-    return wallets.map((wallet) => ({
+
+    const mappedWallet =  wallets.map((wallet) => ({
       address: wallet.address,
       description: wallet.description,
       favorite: wallet.favorite,
+      firstTransaction: wallet.firstTransaction,
+      balance: 0
     }));
+
+    for (const wallet of mappedWallet) 
+    {
+        var result = await this.getWalletBalanceWithEtherscan(wallet.address);
+        if(!isNaN(result))
+            wallet.balance = result;        
+    }
+
+    return mappedWallet;
   }
 
   async getWallet(walletAddress: string) {
@@ -72,6 +56,7 @@ export class WalletsService {
       address: wallet.address,
       description: wallet.description,
       favorite: wallet.favorite,
+      firstTransaction: wallet.firstTransaction
     };
   }
 
@@ -99,5 +84,45 @@ export class WalletsService {
       throw new NotFoundException('Wallet not found');
     }
     return wallet;
+  }
+
+
+  async getWalletBalanceWithEtherscan(address: string) {
+    var balance : number;
+    const url =
+      'https://api.etherscan.io/api?module=account&action=balance&address=' +
+      address +
+      '&tag=latest&apikey=' +
+      this.apiKEY +
+      '';
+    const wallet = await this.httpService.get<AxiosResponse<BasicResult>>(url);
+    await wallet.forEach((w) => {
+        const rawBalance = (w.data as unknown as BasicResult)?.result;
+        balance = parseInt(rawBalance) / this.weiToEth;
+    });
+
+    return balance; 
+  }
+
+  async getWalletFirstUseWithEtherscan(address: string) : Promise<Date> {
+    const url =
+      'https://api.etherscan.io/api?module=account&action=txlist&address=' +
+      address +
+      '&startblock=0&endblock=99999999&sort=asc&apikey=' +
+      this.apiKEY +
+      '';
+      
+    var date : Date = null;
+    const wallet = await this.httpService.get<AxiosResponse<WalletResult>>(url);
+    await wallet.forEach((w) => {   
+        const walletData = w.data as unknown as WalletResult;
+        if(walletData?.message === "NOTOK")
+            throw new NotFoundException("Wallet address format is wrong");
+
+        const firstUse = walletData?.result[0]?.timeStamp;
+        if(firstUse)
+            date = new Date(parseInt(firstUse)*1000);
+    });
+    return (date); 
   }
 }
