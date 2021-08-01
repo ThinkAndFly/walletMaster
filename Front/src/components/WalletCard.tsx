59707a4954b22fa8fbc112794d28bed9f99d1fd8
Juan Di Minico <juandiminico@gmail.com>
2021-08-01 17:14:09 +0200

    added wallet is old logic

--- Front/src/components/WalletCard.tsx
+++ Front/src/components/WalletCard.tsx
@@ -2,20 +2,49 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import walletModel from "../models/wallet.model";
+import { useState } from "react";
+import { useEffect } from "react";
 
 interface IProps {
     wallet: walletModel
 }
 
+const WalletIsOld = (firstTransaction: Date): boolean => {
+    if (!firstTransaction)
+        return false;
+
+    let date = new Date();
+    date.setFullYear(date.getFullYear() - 1);
+
+    if (new Date(firstTransaction) < date)
+        return true
+
+    return false;
+};
+
 const WalletCard = (props: IProps) => {
-    return (
-        <div className="col-sm-5 offset-sm-1 mx-auto">
-            <div className="row">
+
+    let oldblock = () => {
+        if (WalletIsOld(props.wallet.firstTransaction)) {
+            return (
                 <div className="alert alert-danger" role="alert">
                     <FontAwesomeIcon icon={faExclamationTriangle} />
                     Wallet is old!
                 </div>
+            )
+        }
+        else{
+            return ( <div className="alert alert-success" role="alert">
+                <FontAwesomeIcon icon={faExclamationTriangle} />
+                    Wallet is new!
+            </div>) 
+        }
+    }
 
+    return (
+        <div className="col-sm-5 offset-sm-1 mx-auto">
+            <div className="row">
+                {oldblock()}
                 <div className="col-sm p-4 bg-light rounded box-shadow border">
                     {props.wallet.eurex}
                     <FontAwesomeIcon className="position-relative float-end" icon={faEdit} />
